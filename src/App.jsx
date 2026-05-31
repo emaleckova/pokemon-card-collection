044a6ebe8bef6799a4ec5675906178e354c017f0
EvaMaleckova <eva.maleckova@hhu.de>
2026-05-31 15:41:24 +0200

    Pokemon ID update works, card's border colour not yet resolved

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div>
-      <Card />
+      <Card borderColor="red" pokemonId={150} />
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,11 @@
-const Card = ({ borderColor }) => {
+const Card = ({ borderColor, pokemonId }) => {
   return (
     <div className="card" border-color={borderColor}>
-      <h5>Charmander</h5>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" />
+      <h5>{`Pokemon ${pokemonId}`}</h5>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
+        alt={`Pokemon ${pokemonId}`}
+      />
     </div>
   );
 };
